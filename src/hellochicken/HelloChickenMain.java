c944a48a4f21f749fa59f31a140828b4de49c5d3
monya-9 <lee99714@naver.com>
2020-06-16 11:24:10 +0900

    prohect

--- src/hellochicken/HelloChickenMain.java
+++ src/hellochicken/HelloChickenMain.java
@@ -1,5 +1,21 @@
 package hellochicken;
 
-public class HelloChickenMain {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class HelloChickenMain extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
